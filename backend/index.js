002c9f1f140aeb04478f5d331cd49bef653e5a01
omprakash <kanuparewa371@gmail.com>
2026-03-07 13:45:33 +0530

    index.js changed

--- backend/index.js
+++ backend/index.js
@@ -1,30 +1,26 @@
 import express from "express";
 import emailRoutes from './route/email.route.js'
-import path from 'path'
-import dotenv from 'dotenv'; 
+import dotenv from 'dotenv';
 import cors from 'cors'
 
-const app = express(); 
-const __dirname = path.resolve()
+const app = express();
 
-dotenv.config(); 
+dotenv.config();
 
 const corsOptions = {
-    origin: 'http://localhost:3000', 
-    credentials: true, 
+    origin: 'https://nexoradevs.vercel.app',
+    credentials: true,
 };
+
 app.use(cors(corsOptions))
 
-app.use(express.json({limit:"5mb"})); 
-app.use(express.urlencoded({extended:true})); 
+app.use(express.json({limit:"5mb"}));
+app.use(express.urlencoded({extended:true}));
 
-app.use('/api/email',emailRoutes); 
-app.use(express.static(path.join(__dirname,"/dist")))
+app.use('/api/email', emailRoutes);
 
-app.get("*",(req,res)=>{
-    res.sendFile(path.resolve(__dirname,"frontend","dist","index.html")); 
-})
+const PORT = process.env.PORT || 5000;
 
-app.listen(5000,()=>{
-    console.log("Server is running on port 5000");
-})
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
